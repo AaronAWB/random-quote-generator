01d47b4d2e4cea304de1f5879b4d919746a0ae03
AaronAWB <aaron.brinckerhoff@gmail.com>
2022-07-31 16:49:52 -0700

    Edits to citation property of each object

--- js/script.js
+++ js/script.js
@@ -15,43 +15,43 @@ const quotes = [
   {
     quote: "Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.",
     source: "Michael Scott",
-    citation: "Season 2, Episode 6: 'The Fight'",
+    citation: "'The Office' Season 2, Episode 6: 'The Fight'",
     year: "2005"
   },
   {
     quote: "I'm not superstitious, but I am a little stitious.",
     source: "Michael Scott",
-    citation: "Season 4, Episode 1: 'Fun Run'",
+    citation: "'The Office' Season 4, Episode 1: 'Fun Run'",
     year: "2007"
   },
   {
     quote: "Me think, why waste time say lot word when few word do trick.",
     source: "Kevin Malone",
-    citation: "Season 8, Episode 2: 'The Incentive'",
+    citation: "'The Office' Season 8, Episode 2: 'The Incentive'",
     year: "2011"
   },
   {
     quote: "I just want to lie on the beach and eat hot dogs. That's all I've ever wanted.",
     source: "Kevin Malone",
-    citation: "Season 3, Episode 22: 'Beach Games'",
+    citation: "'The Office' Season 3, Episode 22: 'Beach Games'",
     year: "2007"
   },
   {
     quote: "And I knew exactly what to do. But in a much more real sense, I had no idea what to do.",
     source: "Michael Scott",
-    citation: "Season 5, Episode 14: 'Stress Relief Part 1'",
+    citation: "'The Office' Season 5, Episode 14: 'Stress Relief Part 1'",
     year: "2009"
   },
   {
     quote: "There's a lot of beauty in ordinary things. Isn't that kind of the point?",
     source: "Pam Beesly",
-    citation: "Season 9, Episode 23: 'Finale'",
+    citation: "'The Office' Season 9, Episode 23: 'Finale'",
     year: "2013"
   },
   {
     quote: "If I don't have some cake soon, I might die.",
     source: "Stanley Hudson",
-    citation: "Season 4, Episode 7: 'Survivor Man'",
+    citation: "'The Office' Season 4, Episode 7: 'Survivor Man'",
     year: "2007"
   },
 ]
