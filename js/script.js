b4ddc90d5ec842c7efbde0478681766325b9e90b
AaronAWB <aaron.brinckerhoff@gmail.com>
2022-07-31 17:02:43 -0700

    Remove Treehouse code comments

--- js/script.js
+++ js/script.js
@@ -3,14 +3,6 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
-/*** 
- * `quotes` array 
-***/
-
 const quotes = [
   {
     quote: "Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.",
@@ -56,19 +48,11 @@ const quotes = [
   },
 ]
 
-/***
- * `getRandomQuote` function
-***/
-
 function getRandomQuote() {
   let randomNumber = Math.floor(Math.random() * quotes.length);
   let randomQuoteSelection = quotes[randomNumber];
   return randomQuoteSelection
 }
-
-/***
- * `printQuote` function
-***/
 
 function printQuote() {
   let randomQuote = getRandomQuote();
@@ -84,13 +68,5 @@ function printQuote() {
   displayQuote += '</p>';
   quote_box.innerHTML = displayQuote;
 }
-
-
-
-
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
